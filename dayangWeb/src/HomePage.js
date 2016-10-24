8db2b22576bfe1d4744a32f2988b4c861fecef9f
apple <1396539283@qq.com>
2016-10-24 16:48:45 +0800

    先提交一下

--- dayangWeb/src/HomePage.js
+++ dayangWeb/src/HomePage.js
@@ -3,17 +3,33 @@ import md5 from "react-native-md5";
 import './HomePage.css';
 
 var HomePage = React.createClass({
+    handleClick: function() {
+        console.info("dianji");
+    },
     render: function() {
         return (
-            //<div className="Container">
-            //    <div className="Container1">
-            //        gwc
-            //    </div>
-            //    <div className="Container2">
-            //        gwc
-            //    </div>
-            //</div>
-            <div className="CSS">
+            <div className="Container1">
+                <div className="LogoImage">
+                    <img src="logo.png" width="200" height="200"/>
+                    <h1>
+                        大羊健康管理                     
+                    </h1>
+                </div>
+                <div className="InputBase">
+                    <input
+                        className="NameInput"
+                        type="text"
+                     />
+                    <input
+                        className="NameInput"
+                        type="text"
+                     />
+                     <button className="Button"
+                         onClick={this.handleClick}
+                         name="button">
+                         登录
+                     </button>
+                </div>
             </div>
         );
     }
